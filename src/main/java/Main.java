4cb1f6596146b69f2318975741f8592c50988b59
cmoldoveanu0206 <cmoldoveanu0206@stud.acs.upb.ro>
2025-10-08 18:56:24 +0300

    Multiple ping redis respond correct one with more than one clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,36 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args){
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
     int port = 6379;
-    try {
-      serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
       serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-//      clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+      System.out.println("Listening on: " + port);
       while (true) {
-        byte[] input = new byte[1024];
-        clientSocket.getInputStream().read(input);
-        String inputString = new String(input).trim();
-        System.out.println("Received" + inputString);
-        clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+        Socket clientSocket = serverSocket.accept();
+        new Thread(() -> handleClient(clientSocket)).start(); //lambda function
       }
-     } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
+    } catch (IOException e) {
+      System.out.println("Error " + e.getMessage());
+    }
+  }
+
+  static void handleClient(Socket client) {
+    try (Socket c = client;
+         InputStream in = c.getInputStream();
+         OutputStream out = c.getOutputStream()) {
+      byte[] buf = new byte[1024];
+      int n;
+      while ((n = in.read(buf)) != 1) {
+        out.write("+PONG\r\n".getBytes());
+        out.flush();
       }
+    } catch (IOException e) {
+      System.out.println("Error" + e.getMessage());
     }
   }
 }
